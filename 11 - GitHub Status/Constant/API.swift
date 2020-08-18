a05ad167ab42f9355f51b7dfb52598f796aa668a
Whoopinstick <k.m.paul68@gmail.com>
2020-08-18 17:22:25 -0400

    initial project setup

---
+++ 11 - GitHub Status/Constant/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  11 - GitHub Status
+//
+//  Created by Kevin Paul on 8/18/20.
+//  Copyright © 2020 Whoopinstick. All rights reserved.
+//
+
+import Foundation
+
+struct API {
+    static let url = "https://kctbh9vrtdwd.statuspage.io/api/v2/summary.json"
+}
